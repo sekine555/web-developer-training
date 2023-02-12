b9e346c60647f649bf5ff1a7b81de078d64f1fb7
sekine555 <s_sekine@se-yan.net>
2023-02-12 23:32:49 +0900

    feat: 共通レイアウト実装

--- 002-develop/web-developer-training-frontend/layouts/default.vue
+++ 002-develop/web-developer-training-frontend/layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div class="flex flex-col gap-6 bg-white text-sm">
     <Header />
-    <slot />
+    <Contents>
+      <slot />
+    </Contents>
   </div>
 </template>
+
+<script setup>
+import Contents from "@/components/helper/Contents/Contents.vue";
+</script>
