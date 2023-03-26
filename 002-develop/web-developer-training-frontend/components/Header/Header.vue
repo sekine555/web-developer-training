4fd44b3a7ef1c6c9a4b0f571eccad53cd9669aa8
tomohiro-iwana <32325795+tomohiro-iwana@users.noreply.github.com>
2023-03-26 23:37:20 +0900

    Headerの見た目をfigmaに合わせる

--- 002-develop/web-developer-training-frontend/components/Header/Header.vue
+++ 002-develop/web-developer-training-frontend/components/Header/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
     <nav
-      className="flex items-center justify-between flex-wrap bg-gray-900 p-2"
+      class="flex items-center justify-between flex-wrap bg-gray-900 p-2 sm:p-4"
     >
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <nuxt-link to="/" className="font-semibold text-xl tracking-tight">
-          Web Developer Training
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <nuxt-link to="/" class="font-semibold text-xl sm:text-2xl tracking-tight">
+          Web Developer Quiz
         </nuxt-link>
       </div>
     </nav>
