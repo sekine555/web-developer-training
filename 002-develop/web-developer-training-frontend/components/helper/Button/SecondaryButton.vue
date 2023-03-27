c682fdde699f5e7f0344bf52a62dce3223809823
sekine555 <s_sekine@se-yan.net>
2023-03-27 10:46:29 +0900

    feat: 共通のボタンコンポーネント作成

---
+++ 002-develop/web-developer-training-frontend/components/helper/Button/SecondaryButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ButtonHTMLAttributes } from "vue";
+
+const { onClick, attributes } = defineProps<{
+  attributes: ButtonHTMLAttributes;
+  onClick?: () => void;
+}>();
+</script>
+
+<template>
+  <button
+    v-bind="attributes"
+    :on-click="onClick"
+    class="items-center w-full h-[45px] text-center rounded text-primary border-primary border-4 bg-white hover:opacity-70 disabled:opacity-30"
+  >
+    <slot />
+  </button>
+</template>
