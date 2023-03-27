bf3eebc3bb109480ee5c06dc119f1e0142d2dba7
sekine555 <s_sekine@se-yan.net>
2023-03-27 09:16:54 +0900

    fix: タイトル追加

--- 002-develop/web-developer-training-frontend/components/Menus/Menus.vue
+++ 002-develop/web-developer-training-frontend/components/Menus/Menus.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Icon from "@/components/helper/Icon/Icon.vue";
+
 const menus = [
   { name: "フロントエンド", imagePath: "/images/menus/frontend.svg" },
   { name: "バックエンド", imagePath: "/images/menus/backend.svg" },
@@ -8,13 +10,18 @@ const menus = [
 </script>
 
 <template>
-  <nav>
-    <ul class="flex items-center justify-center gap-4 flex-wrap sm:flex-nowrap">
-      <li v-for="(menu, index) in menus" :key="index">
-        <a>
-          <Icon :name="menu.name" :path="menu.imagePath" />
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <p class="mt-4 ml-6 font-bold text-2xl pb-6 sm:pb-14">ジャンル</p>
+    <nav>
+      <ul
+        class="flex items-center justify-center gap-4 flex-wrap sm:flex-nowrap"
+      >
+        <li v-for="(menu, index) in menus" :key="index">
+          <a>
+            <Icon :name="menu.name" :path="menu.imagePath" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
