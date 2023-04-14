63eb346ba73fb43f211d28e9bec804c943106622
Yuki-Kurita <258qu1to8yodeg1@gmail.com>
2023-04-14 18:23:16 +0900

    feat(Controller): Create & Return genre response

--- 002-develop/web-developer-training-backend/api/app/controller/genre/GenreController.scala
+++ 002-develop/web-developer-training-backend/api/app/controller/genre/GenreController.scala
@@ -1,19 +1,19 @@
 package controller.genre
 
 import application.genre.GenreService
+import com.google.inject.{ Inject, Singleton }
 import play.api.mvc._
 import play.api.libs.json._
 
-import javax.inject.{ Inject, Singleton }
-
 @Singleton
-class GenreController @Inject() (val cc: ControllerComponents, genreService: GenreService)
+class GenreController @Inject() (val cc: ControllerComponents, val genreService: GenreService)
     extends AbstractController(cc) {
   def getAllGenre() = Action {
-    val genreDtoList = genreService.getGenres
-    val genreResponse = genreDtoList.map(genre => GenreResponse(genre.id.toInt, genre.genreName))
-    // TODO: JSONでレスポンス
-    // [ { id: 1, genreName: "フロントエンド" }, { id: 2, genreName: "バックエンド" } ]
-    Ok(Json.toJson(Map("message" -> "Hello Play")))
+    Ok(
+      Json.toJson(
+        genreService.getGenres
+          .map(genre => GenreResponse(genre.id.toInt, genre.genreName))
+      )
+    )
   }
 }
--- 002-develop/web-developer-training-backend/api/app/controller/genre/GenreResponse.scala
+++ 002-develop/web-developer-training-backend/api/app/controller/genre/GenreResponse.scala
@@ -1,3 +1,9 @@
 package controller.genre
 
+import play.api.libs.json.{ Json, Writes }
+
 case class GenreResponse(id: Int, genreName: String)
+
+object GenreResponse {
+  implicit val writes: Writes[GenreResponse] = Json.writes[GenreResponse]
+}
