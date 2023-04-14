0d2ffbefa3d793823d661b545d12ec688879f774
Yuki-Kurita <258qu1to8yodeg1@gmail.com>
2023-04-14 18:21:28 +0900

    feat(Service): Transform domain

---
+++ 002-develop/web-developer-training-backend/api/app/application/genre/GenreServiceImpl.scala
@@ -0,0 +1,14 @@
+package application.genre
+
+import domain.genre.{ GenreRepository }
+import scalikejdbc._
+
+import javax.inject.Inject
+
+class GenreServiceImpl @Inject() (val genreRepository: GenreRepository) extends GenreService {
+  override def getGenres: List[GenreDto] = {
+    DB localTx { implicit session =>
+      genreRepository.findAll().map(genre => GenreDto(genre.id, genre.genreName))
+    }
+  }
+}
