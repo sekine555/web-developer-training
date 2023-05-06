31510202db547a9d86d3ca66ada8b0dbd472c23f
sekine555 <s_sekine@se-yan.net>
2023-05-06 13:24:38 +0900

    fix: クイズレスポンスに解答プロパティも追加

--- 002-develop/web-developer-training-backend/api/app/controller/quiz/QuizResponse.scala
+++ 002-develop/web-developer-training-backend/api/app/controller/quiz/QuizResponse.scala
@@ -2,7 +2,13 @@ package controller.quiz
 
 import play.api.libs.json.{ Json, Writes }
 
-case class QuizResponse(id: Int, genreId: Int, question: String, options: List[QuizOptionResponse])
+case class QuizResponse(
+    id: Int,
+    genreId: Int,
+    question: String,
+    options: List[QuizOptionResponse],
+    answer: QuizAnswerResponse
+)
 
 object QuizResponse {
   implicit val writes: Writes[QuizResponse] = Json.writes[QuizResponse]
