96b68d0a364eb747910553b54601b96dc3a9845a
sekine555 <s_sekine@se-yan.net>
2023-05-07 13:08:58 +0900

    fix: データベースとインフラが逆になっていたため修正

--- 002-develop/web-developer-training-frontend/utils/getImagePath.ts
+++ 002-develop/web-developer-training-frontend/utils/getImagePath.ts
@@ -4,9 +4,9 @@ export const getImagePath = (genreCode: string): string => {
   } else if (genreCode === "GENRE_002") {
     return "/images/menus/backend.svg";
   } else if (genreCode === "GENRE_003") {
-    return "/images/menus/infrastructure.svg";
-  } else if (genreCode === "GENRE_004") {
     return "/images/menus/database.svg";
+  } else if (genreCode === "GENRE_004") {
+    return "/images/menus/infrastructure.svg";
   } else {
     return "";
   }
